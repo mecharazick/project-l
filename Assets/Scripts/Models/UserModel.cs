601f7fbc61758233ca10a81eec9685d8c895716e
Lucas Pinheiro Moraes <lucas3.14nheiro@gmail.com>
2023-02-21 13:53:54 -0300

    :sparkles: screen flow

--- Assets/Scripts/Models/UserModel.cs
+++
@@ -1,8 +0,0 @@
-namespace AlphaLobby.Models
-{
-        public abstract class User
-        {
-            public string Username { get; set; }
-            public string UserId { get; set; }
-        }
-}
